c24068aadfbccbce02566ad19c9d559cc130557a
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2021-10-02 10:34:30 -0500

    Prove-week3
    Adding json file

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
-
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => { // Home
     Product.fetchAll(products => {
@@ -10,26 +10,64 @@ exports.getProducts = (req, res, next) => { // Home
         
         });
     });
-    
 }
-
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+       res.render('shop/product-detail', {
+           product: product , 
+           pageTitle: product.title,
+           path: '/products'
+        });
+    })
+   
+}
 exports.getIndex = (req, res, next) => {
     Product.fetchAll(products => {
         res.render('shop/index', {
         productList: products,
         pageTitle: 'Shop',
-        path: 'Home',
+        path: '/',
         
         });
     });
 }
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your cart'
+    Cart.getCart(cart => {
+      Product.fetchAll(products => {
+        const cartProducts = [];
+        for (product of products) {
+          const cartProductData = cart.products.find(
+            prod => prod.id === product.id
+          );
+          if (cartProductData) {
+            cartProducts.push({ productData: product, qty: cartProductData.qty });
+          }
+        }
+        res.render('shop/cart', {
+          path: '/cart',
+          pageTitle: 'Your Cart',
+          products: cartProducts
+        });
+      });
     });
-}
+  };
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, product => {
+      Cart.addProduct(prodId, product.price);
+    });
+    res.redirect('/cart');
+};  
+
+exports.postCartDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, product => {
+      Cart.deleteProduct(prodId, product.price);
+      res.redirect('/cart');
+    });
+  };
 exports.getOrders = (req, res, next) => {
     res.render('shop/orders', {
         path: '/orders',
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,9 @@
 const path = require('path');
+
 const express = require('express');
+
 const adminController = require('../controllers/admin');
+
 const router = express.Router();
 
 router.get('/add-product',adminController.getAddProduct);
@@ -10,5 +13,10 @@ router.get('/products', adminController.getProducts);
 
 router.post('/add-product', adminController.postAddProduct);
 //router.post('/remove-book', productsController.deleteProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+//router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
