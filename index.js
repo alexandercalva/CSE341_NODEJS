bddb36ced7870eb093561d9bd0fe11fac6501776
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2021-10-09 14:38:32 -0500

    Update project
    Adding MongoDb to project - week 4

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
 const path = require('path');
+
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-//It's importing form other  js folders
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-//const storeRoutes = require('./routes/admin');
-const errorController = require('./controllers/error');
-app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
+app.use((req, res, next) => {
+  User.findById('6161e43325c8031d802dae17')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+    
+});
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
 app.use(errorController.get404);
 
-app.listen(5000);
+mongoose
+  .connect(
+    'mongodb+srv://Alexander:Enter@cluster0.6lfjd.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Alexander',
+          email: 'alexander@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
